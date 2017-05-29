907bb5c979e28d2b74074b4c08e62bc231756577
Joshua Johnson <joshua@weareenvoy.com>
2017-05-29 16:57:30 -0700

    Fixed issue with virtualenv being unavailable after install.

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -4,7 +4,6 @@ yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc pat
 yum install -y gcc-c++ flex epel-release nginx supervisor
 yum install -y python-pip python35 python35-pip
 /etc/init.d/nginx stop
-
 # Feels dirty - but need virtualenv installed w/ root permissions
 # Piggyback off of root permissions in this script. Move
 # if additional python dependencies need to be installed w/ root.
--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-virtualenv /home/ec2-user/.virtualenvs/ranked-venv --python=/usr/bin/python3
+/usr/local/bin/virtualenv /home/ec2-user/.virtualenvs/ranked-venv --python=/usr/bin/python3
 chown ec2-user:ec2-user /home/ec2-user/.virtualenvs/ranked-venv
 chown ec2-user:ec2-user /home/ec2-user/.virtualenvs/ranked-venv*
 source /home/ec2-user/.virtualenvs/ranked-venv/bin/activate
