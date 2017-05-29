804aeb825b06d4e314c88bd1bb43ac412067a9e1
Joshua Johnson <joshua@weareenvoy.com>
2017-05-29 15:30:56 -0700

    Fixed appSpec structure.

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -5,11 +5,11 @@ mkdir -p /etc/nginx/sites-available
 
 sudo mkdir -p /etc/nginx/log/
 
-sudo cp /home/ec2-user/src/ranked/Conf/nginx/default.conf /etc/nginx/nginx.conf
+sudo cp /home/ec2-user/src/ranked/conf/nginx/default.conf /etc/nginx/nginx.conf
 
 sudo unlink /etc/nginx/sites-enabled/*
 
-sudo cp /home/ec2-user/src/ranked/Conf/nginx/dev.conf /etc/nginx/sites-available/my-project-host.conf
+sudo cp /home/ec2-user/src/ranked/conf/nginx/dev.conf /etc/nginx/sites-available/my-project-host.conf
 
 sudo ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
 
