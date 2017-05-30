7b6d8795f990f9f06c5d9402d8e0e9cac187a741
Joshua Johnson <joshua@weareenvoy.com>
2017-05-29 17:27:51 -0700

    Remove explicit calls to sudo - scripts running as root are enough.

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -3,15 +3,15 @@
 mkdir -p /etc/nginx/sites-enabled
 mkdir -p /etc/nginx/sites-available
 
-sudo mkdir -p /etc/nginx/log/
+mkdir -p /etc/nginx/log/
 
-sudo cp /home/ec2-user/src/ranked/conf/nginx/default.conf /etc/nginx/nginx.conf
+cp /home/ec2-user/src/ranked/conf/nginx/default.conf /etc/nginx/nginx.conf
 
-sudo unlink /etc/nginx/sites-enabled/*
+unlink /etc/nginx/sites-enabled/*
 
-sudo cp /home/ec2-user/src/ranked/conf/nginx/dev.conf /etc/nginx/sites-available/my-project-host.conf
+cp /home/ec2-user/src/ranked/conf/nginx/dev.conf /etc/nginx/sites-available/my-project-host.conf
 
-sudo ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
+ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
 
-sudo /etc/init.d/nginx reload
-sudo /etc/init.d/nginx start
+/etc/init.d/nginx reload
+/etc/init.d/nginx start
