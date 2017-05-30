35d0caad86fbabaa2826c164a0082d96c99fda63
Joshua Johnson <joshua@weareenvoy.com>
2017-05-29 17:09:28 -0700

    Tweaked bash_profile overwrite on deploy.

--- scripts/misc_housekeeping.sh
+++ scripts/misc_housekeeping.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "export WORKON_HOME=$HOME/src/ranked-venv" >> /home/ec2-user/.bash_profile
-echo "export PROJECT_HOME=$HOME/src/ranked/" >> /home/ec2-user/.bash_profile
-echo "export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh" >> /home/ec2-user/.bash_profile
-echo "source /usr/local/bin/virtualenvwrapper_lazy.sh" >> /home/ec2-user/.bash_profile
+
+# Allow for easy virtualenvwrapper support During SSH sessions
+cp -f /home/ec2-user/src/ranked/conf/bash/bash_profile /home/ec2-user/.bash_profile
