0141034e407c1d81eeaae8df4517530b2e022721
Joshua Johnson <joshua@weareenvoy.com>
2017-05-29 17:33:40 -0700

    Remove collectstatic call [No static S3 bucket set yet... will enable at later date]

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/src/ranked/
-source /home/ec2-user/src/ranked-venv/bin/activate
+source /home/ec2-user/.virtualenvs/ranked-venv/bin/activate
 # supervisorctl -c /home/ec2-user/www/project/supervisor/default.conf stop all 2&>1 >/dev/null
 # sudo unlink /tmp/supervisor.sock 2> /dev/null
 pkill supervisor*
