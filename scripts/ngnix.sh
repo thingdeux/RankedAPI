d2bf024ee2e76edd4b84819f10ffb752f7ff8ee1
Joshua Johnson <joshua@weareenvoy.com>
2017-06-01 22:32:58 -0700

    Set uWSGI to run off of socket instead of http port. Updated access/error log locations. Added goranked.ini conf for uWSGI configuration. Added route for generated swagger API docs.

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -9,9 +9,9 @@ cp /home/ec2-user/src/ranked/conf/nginx/default.conf /etc/nginx/nginx.conf
 
 unlink /etc/nginx/sites-enabled/*
 
-cp /home/ec2-user/src/ranked/conf/nginx/dev.conf /etc/nginx/sites-available/my-project-host.conf
+cp /home/ec2-user/src/ranked/conf/nginx/dev.conf /etc/nginx/sites-available/goranked.conf
 
-ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
+ln -s /etc/nginx/sites-available/goranked.conf /etc/nginx/sites-enabled/goranked.conf
 
 /etc/init.d/nginx reload
 /etc/init.d/nginx start
