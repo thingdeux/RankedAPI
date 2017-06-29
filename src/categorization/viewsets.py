1536339c2cd46ac038b9e9902a84a7cd6887cf29
Joshua Johnson <joshua@weareenvoy.com>
2017-06-29 16:05:31 -0700

    Slapped 24 hour cache on /categories/

--- src/categorization/viewsets.py
+++ src/categorization/viewsets.py
@@ -1,6 +1,9 @@
 # DRF Imports
 from rest_framework import permissions, viewsets
 from .serializers import CategorySerializer
+# Django Imports
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 # Project Imports
 from .models import Category
 # Library Imports
@@ -8,6 +11,12 @@ from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    TWENTY_FOUR_HOURS_IN_SECONDS = 86400
+
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(is_active=True)
+
+    @method_decorator(cache_page(TWENTY_FOUR_HOURS_IN_SECONDS))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CategoryViewSet, self).dispatch(request, *args, **kwargs)
