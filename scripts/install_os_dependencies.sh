32b16a643cb4d4019377a854584c182289ec9dfa
Joshua Johnson <joshua@weareenvoy.com>
2017-07-13 20:35:56 -0700

    Added python3 development library to os packages.

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yum install -y python-psycopg2 postgresql libncurses5-dev libffi libffi-devel libxml2-devel libxslt-devel libxslt1-dev
-yum install -y postgresql-libs postgresql-devel python-lxml python-devel gcc patch python-setuptools
+yum install -y postgresql-libs postgresql-devel python-lxml python-devel python35-devel.x86_64 gcc patch python-setuptools
 yum install -y gcc-c++ flex epel-release nginx supervisor
 yum install -y python-pip python35 python35-pip htop
 /etc/init.d/nginx stop
