e91f7f5d79bef830292d8f642c4fb98df5447246
Joshua Johnson <joshua@weareenvoy.com>
2017-07-17 22:48:53 -0700

    Added support for easier admin querying of video items (for maintenance / polish)

--- src/video/admin.py
+++ src/video/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Video
 
 class VideoAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'custom_field1')
     fields = ('title', 'hashtag', 'mobile', 'low', 'high', 'hd', 'thumbnail_small', 'thumbnail_large',
               'category', 's3_filename', 'is_processing', 'pre_signed_upload_url',
               'rank_total', 'is_top_10', 'is_active', 'is_featured', 'custom_field1')
