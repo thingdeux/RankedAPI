4fbc2aff0b54e30d91dbbe83c446858108b60513
Joshua Johnson <joshua@weareenvoy.com>
2017-07-28 05:38:31 -0700

    Added custom ranked properties to user model admin.

--- src/profile/admin.py
+++ src/profile/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from src.profile.models import Profile
 
-admin.site.register(Profile, UserAdmin)
+class ProfileAdmin(UserAdmin):
+    search_fields = ('username',)
+    readonly_fields = ('last_logged_in', 'is_partner', 'is_featured')
+    fieldsets = UserAdmin.fieldsets + (
+        ("Ranked Properties", {'fields': ('avatar_url', 'is_partner', 'is_featured', 'last_logged_in', 'following_count',
+                     'followers_count', 'ranked_ten_count')}),
+    )
+admin.site.register(Profile, ProfileAdmin)
